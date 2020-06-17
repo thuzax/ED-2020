260d1af7d18324bf2559c2b430f55be03aeef062
thuzax <arthurhscruz@gmail.com>
2020-06-17 01:37:47 -0300

    Testes preparados

--- listas-de-exercicios/lista-3/ordenacao/algoritmos_ordenacao.py
+++ listas-de-exercicios/lista-3/ordenacao/algoritmos_ordenacao.py
@@ -4,58 +4,80 @@ def trocar(vetor, i, j):
     vetor[j] = auxiliar
 
 def bubble_sort(vetor):
+    numer_de_passos = 0
+    
     i = 0
+    
     while (i < len(vetor)-1):
+    
         for j in range(i, len(vetor)):
+            numer_de_passos += 1
+    
             if (vetor[i] > vetor[j]):
                 trocar(vetor, i, j)
         i += 1
     
-    return vetor
+    return (vetor, numer_de_passos)
 
 
 
 def merge(primeira_metade, segunda_metade):
+    numero_de_passos = 0
+    
     resultado = []
 
     i = 0
     j = 0
 
+
     while (i < len(primeira_metade) and j < len(segunda_metade)):
+        numero_de_passos += 1
+        
         if (primeira_metade[i] < segunda_metade[j]):
             resultado.append(primeira_metade[i])
             i += 1
+        
         else:
             resultado.append(segunda_metade[j])
             j += 1
 
     while (i < len(primeira_metade)):
+        numero_de_passos += 1
+        
         resultado.append(primeira_metade[i])
         i += 1
     
     while (j < len(segunda_metade)):
+        numero_de_passos += 1
+        
         resultado.append(segunda_metade[j])
         j += 1
 
-    return resultado
+    return (resultado, numero_de_passos)
 
 def merge_sort(vetor):
     if (len(vetor) == 1):
-        return vetor
+        return (vetor, 1)
     
+    numero_de_passos = 0
+
     meio = int(len(vetor)/2)
     primeira_metade = vetor[:meio]
     segunda_metade = vetor[meio:]
     
-    primeira_metade = merge_sort(primeira_metade)
-    segunda_metade = merge_sort(segunda_metade)
+    primeira_metade, num_de_passos_1 = merge_sort(primeira_metade)
+    segunda_metade, num_de_passos_2 = merge_sort(segunda_metade)
     
-    vetor = merge(primeira_metade, segunda_metade)
+    vetor, num_de_passos_merge = merge(primeira_metade, segunda_metade)
     
-    return vetor
+    numero_de_passos += num_de_passos_1 + num_de_passos_2 + num_de_passos_merge
+
+    return (vetor, numero_de_passos)
 
 
 def make_quick_sort(vetor, inicio, fim):
+    numero_de_passos = 0
+
     i = inicio
     j = fim
     meio = int((inicio + fim) / 2)
@@ -64,24 +86,34 @@ def make_quick_sort(vetor, inicio, fim):
     while (i < j):
 
         while (i < fim and vetor[i] < pivo):
+            numero_de_passos += 1
+            
             i += 1
         
         while (j > inicio and vetor[j] > pivo):
+            numero_de_passos += 1
+            
             j -= 1
 
         if (i <= j):
+            numero_de_passos += 1
+
             trocar(vetor, i, j)
             i += 1
             j -= 1
 
 
+    num_passos_1 = 0
+    num_passos_2 = 0
     if (inicio < j):
-        vetor = make_quick_sort(vetor, inicio, j)
+        vetor, num_passos_1 = make_quick_sort(vetor, inicio, j)
 
     if (fim > i):
-        vetor = make_quick_sort(vetor, i, fim)
+        vetor, num_passos_2 = make_quick_sort(vetor, i, fim)
+    
+    numero_de_passos += num_passos_1 + num_passos_2
 
-    return vetor
+    return (vetor, numero_de_passos)
 
 
 def quick_sort(vetor):
--- listas-de-exercicios/lista-3/ordenacao/main.py
+++ listas-de-exercicios/lista-3/ordenacao/main.py
@@ -1,24 +1,163 @@
 import random
+import time
 
 from algoritmos_ordenacao import bubble_sort
 from algoritmos_ordenacao import merge_sort
 from algoritmos_ordenacao import quick_sort
 
+def executa_teste(vetor):
+    vetor_teste = vetor
+    
+    tempo_ini = time.time()
+    resultado_bubble = bubble_sort(vetor_teste)
+    tempo_fim = time.time()
+    tempo_bubble = tempo_fim - tempo_ini
+
+    vetor_teste = vetor
+    
+    tempo_ini = time.time()
+    resultado_merge = merge_sort(vetor_teste)
+    tempo_fim = time.time()
+    tempo_merge = tempo_fim - tempo_ini
+    
+    vetor_teste = vetor
+    
+    tempo_ini = time.time()
+    resultado_quick = quick_sort(vetor_teste)
+    tempo_fim = time.time()
+    tempo_quick = tempo_fim - tempo_ini
+    
+    tempos = (tempo_bubble, tempo_merge, tempo_quick)
+
+    return (resultado_bubble, resultado_merge, resultado_quick, tempos)
+
+
+
 
 if __name__=="__main__":
-    vetor_desordenado = [random.randint(0,100) for i in range(20)]
+    vets_desordenado = []
+    vets_bubble = []
+    vets_merge = []
+    vets_quick = []
+    vet_passos_bubble = []
+    vet_passos_merge = []
+    vet_passos_quick = []
+    vet_tempos_bubble = []
+    vet_tempos_merge = []
+    vet_tempos_quick = []
     
-    print(vetor_desordenado)
+    for i in [100, 1000, 10000]:
+        print("Criando vetor tamanho " + str(i) + "\n")
+        vetor = random.sample(range(0, i*100), i)
+        vets_desordenado.append(vetor)
 
-    vetor_teste = vetor_desordenado
+        print("Iniciando testes")
+        res_bubble, res_merge, res_quick, tempos = executa_teste(vetor)
+        
+
+        vet_bubble, passos_bubble = res_bubble
+        vet_merge, passos_merge = res_merge
+        vet_quick, passos_quick = res_quick
+        tempo_bubble, tempo_merge, tempo_quick = tempos
+
+        vets_bubble.append(vet_bubble)
+        vets_merge.append(vet_merge)
+        vets_quick.append(vet_quick)
+
+        vet_passos_bubble.append(passos_bubble)
+        vet_passos_merge.append(passos_merge)
+        vet_passos_quick.append(passos_quick)
+
+        vet_tempos_bubble.append(tempo_bubble)
+        vet_tempos_merge.append(tempo_merge)
+        vet_tempos_quick.append(tempo_quick)
+
+
+    # ----------------------------- Resultados ---------------------------------
+    print("Vetor desordenado (100)")
+    print(vets_desordenado[0])
+
+    print("Resultado bubble sort (100)")
+    print(vets_bubble[0])
+
+    print("Resultado merge sort (100)")
+    print(vets_merge[0])
+
+    print("Resultado quick sort (100)")
+    print(vets_quick[0])
     
-    vetor_teste = bubble_sort(vetor_teste)
-    print(vetor_teste)
+    print()
 
-    vetor_teste = vetor_desordenado
-    vetor_teste = merge_sort(vetor_teste)
-    print(vetor_teste)
+    print("Vetor desordenado (1000)")
+    print(vets_desordenado[1])
+    
+    print("Resultado bubble sort (1000)")
+    print(vets_bubble[1])
 
-    vetor_teste = vetor_desordenado
-    vetor_teste = quick_sort(vetor_teste)
-    print(vetor_teste)
+    print("Resultado merge sort (1000)")
+    print(vets_merge[1])
+
+    print("Resultado quick sort (1000)")
+    print(vets_quick[1])
+
+    print()
+
+    print("Vetor desordenado (1000000)")
+    print(vets_desordenado[2])
+
+    print("Resultado bubble sort (1000000)")
+    print(vets_bubble[2])
+    
+    print("Resultado merge sort (1000000)")
+    print(vets_merge[2])
+
+    print("Resultado quick sort (1000000)")
+    print(vets_quick[2])
+
+    print()
+
+    # ----------------------------- Estatisticas -------------------------------
+
+    print("Passos bubble sort (100)")
+    print(vet_passos_bubble[0])
+    print("Tempo: " + str(vet_tempos_bubble[0]) + "\n")
+
+    print("Passos merge sort (100)")
+    print(vet_passos_merge[0])
+    print("Tempo: " + str(vet_tempos_merge[0]) + "\n")
+
+    print("Passos quick sort (100)")
+    print(vet_passos_quick[0])
+    print("Tempo: " + str(vet_tempos_quick[0]) + "\n")
+
+    print()
+
+    print("Passos bubble sort (1000)")
+    print(vet_passos_bubble[1])
+    print("Tempo: " + str(vet_tempos_bubble[1]) + "\n")
+
+    print("Passos merge sort (1000)")
+    print(vet_passos_merge[1])
+    print("Tempo: " + str(vet_tempos_merge[1]) + "\n")
+
+    print("Passos quick sort (1000)")
+    print(vet_passos_quick[1])
+    print("Tempo: " + str(vet_tempos_quick[1]) + "\n")
+
+    print()
+
+    print("Passos bubble sort (100000)")
+    print(vet_passos_bubble[2])
+    print("Tempo: " + str(vet_tempos_bubble[2]) + "\n")
+
+    print("Passos merge sort (100000)")
+    print(vet_passos_merge[2])
+    print("Tempo: " + str(vet_tempos_merge[2]) + "\n")
+
+    print("Passos quick sort (100000)")
+    print(vet_passos_quick[2])
+    print("Tempo: " + str(vet_tempos_quick[2]) + "\n")
+
+    print()
+
+
